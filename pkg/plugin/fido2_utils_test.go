a92e3c0cfbafe28572c617b63f5af8134ed70bc9
sebastian <sebastian>
2025-01-03 19:27:02 +0100

    fix: fix tests

--- pkg/plugin/fido2_utils_test.go
+++ pkg/plugin/fido2_utils_test.go
@@ -36,12 +36,12 @@ func TestEnforcePin(t *testing.T) {
 		t.Error(err)
 	}
 
-	secretPin, err := GetHmacSecret(dev, i.CredId, i.Salt, "1234")
+	secretPin, err := getHmacSecret(dev, i.CredId, i.Salt, "1234")
 	if err != nil {
 		t.Error(err)
 	}
 
-	secretNoPin, err := GetHmacSecret(dev, i.CredId, i.Salt, "")
+	secretNoPin, err := getHmacSecret(dev, i.CredId, i.Salt, "")
 	if err != nil {
 		t.Error(err)
 	}
